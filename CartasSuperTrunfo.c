714b926a9daa8d2e2f13c176a379795347f483ec
mark7prog <marcosservidor.video@gmail.com>
2025-02-10 21:38:26 +0000

    Criei o código de cadastro.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,22 +1,61 @@
 #include <stdio.h>
 
-// Desafio Super Trunfo - Países
-// Tema 1 - Cadastro das Cartas
-// Este código inicial serve como base para o desenvolvimento do sistema de cadastro de cartas de cidades.
-// Siga os comentários para implementar cada parte do desafio.
-//Teste larissa
-
 int main() {
-    // Sugestão: Defina variáveis separadas para cada atributo da cidade.
-    // Exemplos de atributos: código da cidade, nome, população, área, PIB, número de pontos turísticos.
-    
-    // Cadastro das Cartas:
-    // Sugestão: Utilize a função scanf para capturar as entradas do usuário para cada atributo.
-    // Solicite ao usuário que insira as informações de cada cidade, como o código, nome, população, área, etc.
-    
-    // Exibição dos Dados das Cartas:
-    // Sugestão: Utilize a função printf para exibir as informações das cartas cadastradas de forma clara e organizada.
-    // Exiba os valores inseridos para cada atributo da cidade, um por linha.
+
+    int populacao, pontost;
+    float area, pib;
+    char pais[30], estado[20], cidade[20], codigo[20];
+
+    printf("CADASTRO SUPER TRUNFO\n\n");
+
+// Cadastro das informações dos Países, Estados e Cidades.
+// Leitura de dados informados.
+
+    printf("Digite o Nome do País:\n");
+    scanf("%s", &pais);
+
+    printf("Digite o Nome do Estado:\n");
+    scanf("%s", &estado);
+
+    printf("Digite o Nome da Cidade:\n");
+    scanf("%s", &cidade);
+
+    printf("Digite o Código da Cidade:\n");
+    scanf("%s", &codigo);
+
+    printf("Digite a População do Cidade:\n");
+    scanf("%d", &populacao);
+
+    printf("Digite o PIB da Cidade:\n");
+    scanf("%f", &pib);
+
+    printf("Digite a quantidade de Pontos Turisticos da Cidade:\n");
+    scanf("%d", &pontost);
+
+    printf("Digite a Área da Cidade:\n");
+    scanf("%f", &area);
+
+// Exibição dos valores cadastrados.
+
+    printf("Cartas Super-Trunfo\n\n");
+
+//Váriaveis tipo CHAR.
+
+    printf("Nome do País: %s\n", pais);
+    printf("Nome do Estado: %s\n", estado);
+    printf("Nome da Cidade: %s\n", cidade);
+    printf("Código da Cidade: %s\n", codigo);
+
+//Váriaveis do tipo INT.
+
+    printf("População: %d habitantes\n", populacao);
+    printf("Pontos Turísticos: %d\n", pontost);
+
+//Váriaveis do tipo DOUBLE.
+
+    printf("Área: %.3f km²\n", area);
+    printf("PIB: %.3f\n", pib);
+
 
     return 0;
 }
